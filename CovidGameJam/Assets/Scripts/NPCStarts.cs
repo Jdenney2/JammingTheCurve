dca31ee17414a59d2745ccd44b877afcdea4901b
James <jamesdenney2@gmail.com>
2020-09-20 06:38:26 -0500

    Implemented framework of infection system

---
+++ CovidGameJam/Assets/Scripts/NPCStarts.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCStarts : MonoBehaviour
+{
+
+    public bool isInfected = false;
+
+    /*
+    Important note: The defense stat of an NPC is functionally the same as health in the vast
+    majority of games, but it plays an even more signifigant role in the simulation aspect of
+    this game. Defense represents a person's "defense" against Covid19, and as a result should
+    be calculated by large number of variables. When a player attacks a healthy NPC, it will drop
+    their defense, and once this stat reaches 0, an NPC is considered "infected," and can no loger be
+    attacked. This stat should also be dropped by other things, such as the breaking of social distancing
+    measures, but for the sake of gameplay, should never be reduced to 0 unless the player attacks the target.
+    */
+
+    public int defense;
+
+    //Values used to calculate the defense of the NPC
+    public bool hasMask;
+
+    
+    //Refernce used to allow the payer to target NPC's head with as little work as possible
+    public Transform head;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        calcuateDefense();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void calcuateDefense() {
+        /*
+            This function will use a list of values to calculate the defense stat of the NPCs. 
+            Initially, it will only use the presence of a mask to determine it, but as development
+            progresses, more variables wil be added to the calculation.
+        */
+
+        if (hasMask) {
+            defense = 10;
+        }
+        else {
+            defense = 4;
+        }
+    }
+
+}
--- CovidGameJam/Assets/Scripts/PlayerController.cs
+++ CovidGameJam/Assets/Scripts/PlayerController.cs
@@ -6,22 +6,30 @@ public class PlayerController : MonoBehaviour
 {
     //Physics variables, for now they are random, and will need a great deal of tuning
     public float speed = 2.0f;
+    public float atkSpeed = 5f;
     public float gravity = 9.0f;
     public float jumpSpeed = 10.0f;
     public float rotateSpeed = 3.0f;
     private float cameraAngle;
     public float maxCameraRotation = 50.0f;
     private float moveDirY;
+    Quaternion oldRotation;
     Vector3 moveDirection = Vector3.zero;
 
     //Logic variables
-    public bool isGrounded;
+    private bool isGrounded;
+    private bool isAttacking;
 
     //References
     public GameObject pivotPoint;
+    private GameObject target = null;
 
     //Components
     private CharacterController charController;
+
+    //Timing Vars
+    private float curReboundTimer;
+    public float reboundTime = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -29,30 +37,89 @@ public class PlayerController : MonoBehaviour
         charController = GetComponent<CharacterController>();
         cameraAngle = 0;
         moveDirY = 0;
+        isAttacking = false;
+        curReboundTimer = 0;
     }
 
     void Update()
     {        
-//Handle Player Horizontal Rotation
-        transform.Rotate(0, Input.GetAxis("Mouse X") * rotateSpeed, 0);
 
         //Handle Camera Vertical Rotation
         cameraAngle += -Input.GetAxis("Mouse Y") * rotateSpeed;
         cameraAngle = Mathf.Clamp(cameraAngle, -maxCameraRotation, maxCameraRotation);
         pivotPoint.transform.localRotation = Quaternion.AngleAxis(cameraAngle, Vector3.right);
 
-        //Handle Character Movement
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        moveDirection *= speed;
+        if (!isAttacking && curReboundTimer <= 0) {
+            //Handle Player Horizontal Rotation
+            transform.Rotate(0, Input.GetAxis("Mouse X") * rotateSpeed, 0);
 
-        if (charController.isGrounded  && Input.GetButtonDown("Jump")) {
-            moveDirY = jumpSpeed;
+            //Handle Character Movement
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (charController.isGrounded  && Input.GetButtonDown("Jump")) {
+                moveDirY = jumpSpeed;
+            }
+
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirY -= gravity * Time.deltaTime;
+            moveDirection.y = moveDirY;
+
+            charController.Move(moveDirection * Time.deltaTime);
+
+            //If the player hits the attack button, has a target, and can attack, then handle that here
+            if(Input.GetButtonDown("Fire1") && target != null) {
+                oldRotation = transform.rotation;
+                isAttacking = true;
+            }
         }
 
-        moveDirection = transform.TransformDirection(moveDirection);
-        moveDirY -= gravity * Time.deltaTime;
-        moveDirection.y = moveDirY;
+        if (isAttacking) {
+            transform.LookAt(target.GetComponent<NPCStarts>().head);
 
-        charController.Move(moveDirection * Time.deltaTime);
+            moveDirection = new Vector3(0, 0, 1);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= atkSpeed;
+
+            charController.Move(moveDirection * Time.deltaTime);
+
+            //If distance is below a certain threshold, damage the targets defense then begin to bounce backwards
+            //Note: May have to tinker with the threshold value
+            if (Vector3.Distance(transform.position, target.GetComponent<NPCStarts>().head.position) <= 0.7) {
+                target.GetComponent<NPCStarts>().defense -= 1;
+                if(target.GetComponent<NPCStarts>().defense == 0) {
+                    target.GetComponent<NPCStarts>().isInfected = true;
+                }
+
+                isAttacking = false;
+                curReboundTimer = reboundTime;
+            }
+        }
+
+        if (curReboundTimer > 0) {
+            moveDirection = new Vector3(0, 0, -1);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= atkSpeed;
+
+            charController.Move(moveDirection * Time.deltaTime);
+            curReboundTimer -= Time.deltaTime;
+
+            if (curReboundTimer <= 0) {
+                //When done rebounding, reset the rotation
+                transform.rotation = oldRotation;
+            }
+        }
+
     }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "NPC" && !other.gameObject.GetComponent<NPCStarts>().isInfected) {
+            target = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        target = null;
+    }
+
 }
