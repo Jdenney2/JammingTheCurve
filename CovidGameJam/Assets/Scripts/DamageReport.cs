b31cce6bef9ca30e989f18055ed2509237aa975c
chris brown <sevrc1136@gmail.com>
2020-09-20 14:30:37 -0500

    finished timer and damage report, need to add more to report

---
+++ CovidGameJam/Assets/Scripts/DamageReport.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageReport : MonoBehaviour
+{   
+    public Text title, peopleInfected, objects_with_covid;
+    private int PI = 0, OWC = 0; //peopleInfected and objects with covid
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        title.text = "";
+        peopleInfected.text = "";
+        objects_with_covid.text = "";
+    }
+
+    public void ShowResults()
+    {
+        CalculateObjects();
+        CalculatePeople();
+
+        title.text = "Report";
+        peopleInfected.text = "People Infected :\n" + PI;
+        objects_with_covid.text = "Objects with Covid:\n" + OWC;
+    }
+
+    private void CalculatePeople()
+    {
+        GameObject[] objs;
+
+        objs = GameObject.FindGameObjectsWithTag("NPC");
+
+        Debug.Log("length of ppl " + objs.Length);
+
+        // foreach(GameObject go in objs)
+        // {
+        //     if(go.GetComponent<NPCStarts>().isInfected == true)
+        //     {
+        //         PI++;
+        //     }
+        // }
+    }
+
+    private void CalculateObjects()
+    {
+        GameObject[] objs;
+
+        objs = GameObject.FindGameObjectsWithTag("Object");
+
+        Debug.Log("length of objs " + objs.Length);
+
+        foreach(GameObject go in objs)
+        {
+            if(go.GetComponent<InfectionSwitch>().infected == true)
+            {
+                OWC++;
+            }
+        }
+    }
+}
---
+++ CovidGameJam/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    public float timer = 60.0f;
+    public DamageReport damageReport;
+    private Text text;
+    private bool timeUp = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = this.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer < 0 && timeUp == false)
+        {
+            timer = 0.0f;
+            text.text = (timer).ToString("0");
+            timeUp = true;
+            damageReport.ShowResults();
+        }
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            text.text = (timer).ToString("0");
+        }
+    }
+
+}
