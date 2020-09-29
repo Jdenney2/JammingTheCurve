3b51212f858ddda2f9e36ed6ecd4c45fe823fd5d
chris brown <sevrc1136@gmail.com>
2020-09-29 02:11:06 -0500

    added UI function and change shield color

---
+++ CovidGameJam/Assets/Scripts/PlayerRay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerRay : MonoBehaviour
+{
+    public int len;
+    public Text shieldStr;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward * len);
+        Debug.DrawRay(transform.position, forward, Color.red);
+
+        if(Physics.Raycast(transform.position, forward, out hit))
+        {
+            if(hit.collider.tag == "NPC")
+            {
+                shieldStr.text = hit.collider.GetComponent<NPCStarts>().defense.ToString();
+            }
+        }
+
+    }
+}
--- CovidGameJam/Assets/Scripts/Shield.cs
+++ CovidGameJam/Assets/Scripts/Shield.cs
@@ -1,26 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class Shield : MonoBehaviour
 {
-    public Text shieldStr;
-    Renderer rend; 
+    public NPCStarts stats;
+    Material m_Material;
     // Start is called before the first frame update
     void Start()
     {
-        rend = GetComponent<Renderer>();
+        m_Material = GetComponent<Renderer>().material;
     }
-
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(0.0f, 90.0f * 1.0f * Time.deltaTime, 0.0f, Space.Self);
 
-        /*
-            here will be an if / else changing color of infected or not
-            this will also bridge and change UI text for shield str
-        */
+        if(stats.isInfected)
+            m_Material.color = Color.green;
+        else
+            m_Material.color = Color.blue;
     }
 }
