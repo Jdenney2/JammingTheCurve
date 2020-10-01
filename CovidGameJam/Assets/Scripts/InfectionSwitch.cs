35aa688d3d8f97b0d370f0745e31a19f618f0097
chris brown <sevrc1136@gmail.com>
2020-10-01 14:06:50 -0500

    finished, ready for sound and criticism

--- CovidGameJam/Assets/Scripts/InfectionSwitch.cs
+++ CovidGameJam/Assets/Scripts/InfectionSwitch.cs
@@ -6,6 +6,18 @@ public class InfectionSwitch : MonoBehaviour
 {
     public bool infected = false;
 
+    Material m_Material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Material = GetComponent<Renderer>().material;
+    }
+    void Update()
+    {
+        if(infected)
+            m_Material.color = Color.green;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Employee")
